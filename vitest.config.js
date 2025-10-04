c5f6c73efe70f8d04742026b86b44a7c9afb9311
gabriel <villar.gabriel.p.6@gmail.com>
2025-10-04 15:29:12 +0800

    :wrench: configured test coverage

--- vitest.config.js
+++ vitest.config.js
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
@@ -9,6 +10,19 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      coverage: {
+        provider: 'v8', // or 'v8'
+        reporter: ['text'], // output formats
+        reportsDirectory: './coverage',
+        include: ['src/**/*.{js,vue}'], // only measure these files
+        exclude: [
+          'src/main.js', // entry file
+          'src/**/*.d.ts', // type definitions (if any)
+          'src/**/__tests__/**', // test files
+          'src/**/mocks/**', // mocks
+          'src/setupTests.js', // test setup
+        ],
+      },
     },
   }),
 )
